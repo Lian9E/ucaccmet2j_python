04c01ab3e9a06445207f93c35a11cf307d9a8a4c
Lian Evers <l.x.x.evers@students.uu.nl>
2022-01-11 16:17:00 +0100

    part 1 if-statements modified

--- code_part_1.py
+++ code_part_1.py
@@ -32,38 +32,16 @@ for dict in seatle_station:
     Month_values.append(short_list)
 
 #create a empty list    
-sum_Month = [0,0,0,0,0,0,0,0,0,0,0,0]
+sum_Month =[0,0,0,0,0,0,0,0,0,0,0,0,]
 
 #for each month add the sum to the list
 for pair in Month_values:
-    if pair[0] == '01':
-        sum_Month[0] = sum_Month[0] + pair[1]    
-    if pair[0] == '02':
-        sum_Month[1] = sum_Month[1] + pair[1]
-    if pair[0] == '03':
-        sum_Month[2] = sum_Month[2] + pair[1]
-    if pair[0] == '04':
-        sum_Month[3] = sum_Month[3] + pair[1]
-    if pair[0] == '05':
-            sum_Month[4] = sum_Month[4] + pair[1]
-    if pair[0] == '06':
-        sum_Month[5] = sum_Month[5] + pair[1]
-    if pair[0] == '07':
-        sum_Month[6] = sum_Month[6] + pair[1]
-    if pair[0] == '08':
-        sum_Month[7] = sum_Month[7] + pair[1]
-    if pair[0] == '09':
-        sum_Month[8] = sum_Month[8] + pair[1]
-    if pair[0] == '10':
-        sum_Month[9] = sum_Month[9] + pair[1]
-    if pair[0] == '11':
-        sum_Month[10] = sum_Month[10] + pair[1]
-    if pair[0] == '12':
-        sum_Month[11] = sum_Month[11] + pair[1]
+    sum_Month[int(pair[0])-1] += pair[1]
+   
 print(sum_Month)
 
-with open('monthly_precipitation.json', 'w') as monthly_p_file:
-    json.dump(sum_Month, monthly_p_file)
+with open('monthly_precipitation.json', 'w') as monthly_precipitation:
+    json.dump(sum_Month, monthly_precipitation)
     
 
   
